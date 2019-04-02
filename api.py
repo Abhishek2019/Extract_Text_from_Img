126a46c068efff6decd0096f6ae7f4123041ece2
Abhishek2019 <abhishekw13@gmail.com>
2019-04-02 18:10:38 +0800

    flask wrapper added in api.py file

--- api.py
+++ api.py
@@ -1,28 +1,43 @@
 from PIL import Image
-
-
-
-from PIL import Image
-import sys
-
-import pyocr
 import pyocr.builders
+from flask import Flask, request
+from flask_restful import Resource, Api
+import base64
+
+# with open("testImg/test_1.jpg", "rb") as image_file:
+#     img_data = base64.b64encode(image_file.read())
+# print(img_data)
+
+app = Flask(__name__)
+api = Api(app)
 
 tools = pyocr.get_available_tools()
-# The tools are returned in the recommended order of usage
 tool = tools[0]
-
 langs = tool.get_available_languages()
 lang = langs[0]
-# Note that languages are NOT sorted in any way. Please refer
-# to the system locale settings for the default language
-# to use.
 
 
-txt = tool.image_to_string(
-    Image.open('testImg/road.jpg'),
-    lang=lang,
-    builder=pyocr.builders.TextBuilder()
-)
-print(txt)
+class ExtractImg(Resource):
+
+    def post(self):
+        req_data = request.get_json()
+
+        img_data = req_data["img"]
+
+        with open("testImg/imageToSave.png", "wb") as fh:
+            fh.write(base64.decodebytes(img_data))
+
+        txt = tool.image_to_string(
+                Image.open("testImg/imageToSave.png"),
+                lang=lang,
+            builder=pyocr.builders.TextBuilder()
+        )
+
+        return ({"output":txt})
+
+
+api.add_resource(ExtractImg,"/getText")
+
+app.run(host='127.0.0.1', port = '2935', debug = True)
+
 
